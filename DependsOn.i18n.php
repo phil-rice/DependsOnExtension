32c81ca5ba1afff4b86c24dc164fcfd171bc9bd8
Phil <phil@V2V3Dev.(none)>
2013-02-12 19:46:35 +0000

    Initial Commit

---
+++ DependsOn.i18n.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Internationalization file.
+ */
+ 
+$magicWords = array();
+ 
+$magicWords['en'] = array(
+   'dependsOn' => array( 0, 'dependsOn' ),
+);
---
+++ DependsOn.php
@@ -0,0 +1,37 @@
+<?php
+
+global $wgDependsOnRootDirectory;
+
+// Take credit for your work.
+$wgExtensionCredits['parserhook'][] = array(
+		'path' => __FILE__,
+		// The name of the extension, which will appear on Special:Version.
+		'name' => 'DependsOn ParserFunction',
+		'description' => 'Shows the projects that this project depends on',
+		'version' => 1,
+
+		// Your name, which will appear on Special:Version.
+		'author' => 'Phil Rice',
+
+		// The URL to a wiki page/web page with information about the extension,
+		// which will appear on Special:Version.
+		'url' => 'https://www.softwarefm.com/Wiki/Manual:DependsOnExtension',
+);
+
+$dir = dirname( __FILE__ );
+
+$wgHooks['ParserFirstCallInit'][] = 'DependsOnSetupParserFunction';
+$wgExtensionMessagesFiles['DependsOn'] = $dir . '/DependsOn.i18n.php';
+
+function DependsOnSetupParserFunction( &$parser ) {
+	$parser->setFunctionHook( 'dependsOn', 'DependsOnExtensionRenderParserFunction' );
+	return true;
+}
+
+function DependsOnExtensionRenderParserFunction( $parser, $groupId, $artifactId) {
+	global $wgDependsOnRootDirectory;
+	$fileName = $wgDependsOnRootDirectory . str_replace(".", "/", $groupId) . "/" . $artifactId . "/dependsOn";
+	$dependsOnText = file_get_contents($fileName);
+	$output = "id : [" . $groupId . ":" . $artifactId . "] root = [" . $wgDependsOnRootDirectory . " filename = " . $fileName ."<br />" . $dependsOnText;
+	return $output;
+}
